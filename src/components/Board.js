658f40f150d0716e500cbcf767fe57bf48f41c82
Larry Motuzis <larrymotuzis@Larrys-MacBook-Pro.local>
2023-07-03 15:39:09 -0500

    updated styling

--- src/components/Board.js
+++ src/components/Board.js
@@ -14,18 +14,21 @@ const Board = () => {
     }
   };
 
-    return (
-      <div className="flex flex-col justify-center items-center h-screen bg-gray-100">
-        <div className="grid grid-cols-3 gap-4">
-          {gameState.map((cell, index) => (
-            <button onClick={() => handleCellClick(index)} className="w-24 h-24 border border-gray-300 bg-white flex justify-center items-center text-4xl font-bold" key={index}>
-              {cell}
-            </button>
-          ))}
-        </div>
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-gray-900">
+      <div className="grid grid-cols-3 gap-4">
+        {gameState.map((cell, index) => (
+          <button
+            onClick={() => handleCellClick(index)}
+            className={`w-24 h-24 border border-gray-700 bg-gradient-to-br from-purple-500 to-indigo-500 flex justify-center items-center text-4xl font-bold text-white hover:opacity-75 transition duration-300 ${cell === 'X' ? 'text-green-400' : 'text-red-400'}`}
+            key={index}
+          >
+            {cell}
+          </button>
+        ))}
       </div>
-    );
-  }
-  
-  export default Board;
-  
+    </div>
+  );
+}
+
+export default Board;
