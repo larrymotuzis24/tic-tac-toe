7b5f7b3bf07aee290e46fec3821d5aa110549a5f
Larry Motuzis <larrymotuzis@Larrys-MacBook-Pro.local>
2023-07-03 20:44:34 -0500

    updated styles

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Board from './components/Board';
-import { setPlayerName } from './store/gameSlice';
-import { useDispatch } from 'react-redux';
 
 function App() {
-  const dispatch = useDispatch();
-
-  const [playerName, setPlayer] = useState('');
-
-  const handleInputChange = (e) => {
-    setPlayer(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Perform any action with the submitted name, such as dispatching an action or updating the state
-    dispatch(setPlayerName(playerName))
-  };
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold mb-6">Tic Tac Toe</h1>
-      <div className="max-w-xl">
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="playerName" className="text-xl mb-2">
-            Enter your name:
-          </label>
-          <input
-            type="text"
-            id="playerName"
-            className="border border-gray-300 rounded px-3 py-2 mb-4"
-            value={playerName}
-            onChange={handleInputChange}
-          />
-          <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded">
-            Submit
-          </button>
-        </form>
-        <Board playerName={playerName} />
+      <div className="max-w-4xl p-6 bg-gray-200 rounded-lg">
+        <Board />
       </div>
     </div>
   );
--- src/components/Board.js
+++ src/components/Board.js
@@ -10,14 +10,17 @@ const Board = () => {
   const dispatch = useDispatch();
   const playerName = useSelector(state => state.game.playerName);
   const [isComputerThinking, setIsComputerThinking] = useState(false);
+  const [isUserTurn, setIsUserTurn] = useState(true);
   
   const handleAIMove = () => {
     if (!winner && currentPlayer === 'O') {
+      setIsUserTurn(false);
       setIsComputerThinking(true);
       setTimeout(() => {
         const aiMove = calculateAIMove(gameState);
         dispatch(makeMove({ index: aiMove }));
         setIsComputerThinking(false); 
+        setIsUserTurn(true);
       }, 1000); 
     }
   };
@@ -26,6 +29,7 @@ const Board = () => {
     if (winner) {
       if (winner === 'X') {
         alert(`${playerName} wins`);
+        setIsUserTurn(true);
       } else {
         alert('Computer got you this time');
       }
@@ -39,13 +43,14 @@ const Board = () => {
 
   const handleCellClick = (index) => {
     if (!gameState[index] && !winner) {
+      setIsUserTurn(false);
       dispatch(makeMove({ index }));
     }
   };
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-gray-900">
-      <div className="max-w-xl w-full relative">
+    <div className="flex flex-col justify-center items-center h-screen  mx-auto">
+      <div className="w-full max-w-5xl relative">
         {isComputerThinking ? (
           <div className="flex justify-center absolute bottom-full w-full mb-2">
             <p className="text-white">Computer is thinking...</p>
@@ -62,6 +67,7 @@ const Board = () => {
             onClick={() => handleCellClick(index)}
             className={`w-24 h-24 border border-gray-700 bg-gradient-to-br from-purple-500 to-indigo-500 flex justify-center items-center text-4xl font-bold text-white hover:opacity-75 transition duration-300 ${cell === 'X' ? 'text-green-400' : 'text-red-400'}`}
             key={index}
+            disabled={!isUserTurn || isComputerThinking}
           >
             {cell}
           </button>
